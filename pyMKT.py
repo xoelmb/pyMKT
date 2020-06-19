40895eb2aaa69627ddc8babb14af927a9b0aa09c
Xoel Mato Blanco <xoelmb@gmail.com>
2020-06-19 12:12:55 +0200

    All results OK

--- pyMKT.py
+++ pyMKT.py
@@ -114,7 +114,7 @@ def eMKT(daf, div, cutoff=0.15):
     return res
 
 
-def aMKT(daf, div, xlow=0, xhigh=1):
+def aMKT(daf, div, xlow=0, xhigh=1, check='raise'):
     res = {}
 
     P0 = daf['P0'].sum()
@@ -143,60 +143,79 @@ def aMKT(daf, div, xlow=0, xhigh=1):
 
     try:
         ## Run asymptotic MKT and retrieve alphas
-        model = amkt_fit(daf, div, xlow, xhigh)
+        model = amkt_fit(daf, div, xlow, xhigh, check=check)
         ratio = model['ciHigh'] / model['alpha']
         greater = model['ciHigh'] > model['alpha']
         interval = (1 <= abs(ratio) <= 10)
         if not greater or not interval:
+            # print(model['alpha'], model['ciHigh'])
+            # print("daf20 confidence is too wide")
             raise RuntimeError("daf20 confidence is too wide")
         res.update(model)
         res['daf10'] = False
         # print('LISTO')
 
     except:
-        # print('e')
+        # print('trying daf10')
         daf10 = daf.copy(deep=True)
         daf10['daf'] = np.array([[x / 100, x / 100] for x in range(5, 100, 10)]).flatten()
-
+        daf10 = daf10.groupby('daf', as_index=False).sum()
+        # print(daf10)
         try:
-            model10 = amkt_fit(daf10, div, xlow, xhigh)
-            # UNCOMMENT LINES BELOW TO DISABLE COMPARISON
+            # print('fitting...')
+            model10 = amkt_fit(daf10, div, xlow, xhigh, check=check)
+
+            # print('did fit daf10')
+            # print(model10['alpha'], model10['ciHigh'])
+
+            # COMMENT LINES BELOW TO ENABLE COMPARISON OF DAF20 & DAF10 MODELS
             res.update(model10)
             res['daf10'] = True
 
             # COMMENT LINES BELOW (if-else) TO DISABLE COMPARISON
             # if model is None:
+            #     print('no daf20 model')
             #     res.update(model10)
             #     res['daf10'] = True
             #
             # else:
+            #     # print('comparing models')
             #     ratio10 = model['ciHigh'] / model['alpha']
             #     greater10 = model10['ciHigh'] > model10['alpha']
             #     interval10 = (1 <= abs(ratio10) <= 10)
-            #
+            #     # print(f'G20: {greater};\tI20: {interval};\tG10: {greater10};\tI10: {interval10};\t')
             #     # All cases where 'failed' daf20 is better
             #     if (greater and not greater10) or (not greater and not greater10 and interval and not interval10):
+            #         # print('first condition met: daf20 best')
             #         raise RuntimeError('daf20 was better')
             #
             #     # All cases where daf10 is better
-            #     elif (not greater and greater10) or (greater and greater10 and not interval and interval10) or (not greater and not greater10 and not interval and interval10):
+            #     elif (not greater and greater10) or (greater and greater10 and not interval and interval10) or (
+            #             not greater and not greater10 and not interval and interval10):
+            #         # print('second condition met: daf10 best')
             #         res.update(model10)
             #         res['daf10'] = True
             #
             #     # Tricky cases: both daf10 and 20 do not comply with the two conditions (ratio in interval, ciH>alpha)
             #     elif greater and greater10:  # But none of them in interval
-            #         if model['ciHigh'] < model['ciHigh']:
+            #         # print('third condition met: tricky: not in interval')
+            #         if model['ciHigh'] < model10['ciHigh']:
             #             raise RuntimeError('daf20 was better')
             #         else:
             #             res.update(model10)
             #             res['daf10'] = True
             #
             #     elif interval and interval10:  # But none of them ciH>alpha
+            #         # print('fourth condition met: tricky: not greater')
             #         if ratio > ratio10:
             #             res.update(model10)
             #             res['daf10'] = True
             #         else:
             #             raise RuntimeError('daf20 was better')
+            #     else:
+            #         res.update(model10)
+            #         res['daf10'] = True
+
         except:
             if model is not None:
                 res.update(model)
@@ -217,48 +236,60 @@ def aMKT(daf, div, xlow=0, xhigh=1):
     return res
 
 
-def amkt_fit(daf, div, xlow, xhigh):
+def amkt_fit(daf, div, xlow, xhigh, check='raise'):
     res = {}
 
     d_ratio = float(div['D0'] / div['Di'])
-
+    # print(d_ratio)
     # Compute alpha values and trim
     alpha = 1 - d_ratio * (daf['Pi'] / daf['P0'])
+    # print(alpha)
     trim = ((daf['daf'] >= xlow) & (daf['daf'] <= xhigh))
-    optimize.curve_fit(exp_model, daf['daf'][trim].to_numpy(), alpha[trim].to_numpy(), method='lm')
-    # Two-step nls2() model fit at a given level of precision (res)
 
-
-
+    # Two-step model fit:
+    # First bounded fit:
     try:
-        model = optimize.curve_fit(exp_model, daf['daf'][trim].to_numpy(), alpha[trim].to_numpy(), method='lm')
+        popt, pcov = optimize.curve_fit(exp_model, daf['daf'][trim].to_numpy(), alpha[trim].to_numpy(),
+                                        bounds=([-1, -1, 1], [1, 1, 10]))
+        # print('fit initial')
+    except:
+        # print('could not fit initial')
+        popt = None
+        pcov = None
+
+    # Second fit using initially guessed values or unbounded fit:
+    try:
+        popt, pcov = optimize.curve_fit(exp_model, daf['daf'][trim].to_numpy(), alpha[trim].to_numpy(), p0=popt, method='lm')
         # print('Fit: lm')
     except:
         try:
-            model = optimize.curve_fit(exp_model, daf['daf'][trim].to_numpy(), alpha[trim].to_numpy(), method='trf')
+            popt, pcov = optimize.curve_fit(exp_model, daf['daf'][trim].to_numpy(), alpha[trim].to_numpy(), p0=popt, method='trf')
             # print('Fit: trf')
         except:
             try:
-                model = optimize.curve_fit(exp_model, daf['daf'][trim].to_numpy(), alpha[trim].to_numpy(), method='dogbox')
+                popt, pcov = optimize.curve_fit(exp_model, daf['daf'][trim].to_numpy(), alpha[trim].to_numpy(), p0=popt,
+                                           method='dogbox')
                 # print('Fit: dogbox')
             except:
-                raise RuntimeError("Couldn't fit any method")
+                if not popt:
+                    # print('Could not fit any unbounded')
+                    raise RuntimeError("Couldn't fit any method")
 
-    res['a'] = model[0][0]
-    res['b'] = model[0][1]
-    res['c'] = model[0][2]
+    res['a'] = popt[0]
+    res['b'] = popt[1]
+    res['c'] = popt[2]
 
     # alpha for predicted model
     res['alpha'] = exp_model(1.0, res['a'], res['b'], res['c'])
 
     # Compute confidence intervals based on simulated data (MC-SOERP)
     vcov = pd.concat([pd.DataFrame([0] * 4).transpose(),
-                      pd.concat([pd.DataFrame([0] * 4), pd.DataFrame(model[1])], axis=1, ignore_index=True)],
+                      pd.concat([pd.DataFrame([0] * 4), pd.DataFrame(pcov)], axis=1, ignore_index=True)],
                      axis=0, ignore_index=True)
     vcov = vcov.iloc[0:4, :].values
 
     simpars = np.random.multivariate_normal(mean=[1.0, res['a'], res['b'], res['c']], cov=vcov, size=10000,
-                                            check_valid='raise')  # check_valid=raise -> same as R implementation
+                                            check_valid=check)  # check_valid=raise -> same as R implementation
 
     res['ciLow'], res['ciHigh'] = np.quantile([exp_model(x[0], x[1], x[2], x[3]) for x in simpars], [0.025, 0.975])
 
@@ -270,7 +301,8 @@ def exp_model(f_trimmed, a, b, c):
     return a + b * np.exp(-c * f_trimmed)
 
 
-def mkt_on_df(gene_df, data_df, approach=None, pops=None, tests=None, cutoffs=None, do_trims=None, bootstrap=None, b_size=100, b_reps=100):
+def mkt_on_df(gene_df, data_df, label=None, pops=None, tests=None, cutoffs=None, do_trims=None, bootstrap=None,
+              b_size=100, b_reps=100):
     if do_trims is None:
         do_trims = [True, False]
     if cutoffs is None:
@@ -282,7 +314,8 @@ def mkt_on_df(gene_df, data_df, approach=None, pops=None, tests=None, cutoffs=No
     if bootstrap is None:
         bootstrap = False
 
-    pars = [(gene_df.iloc[:, i], data_df, pops, tests, cutoffs, do_trims, bootstrap, b_size, b_reps) for i in range(len(gene_df.columns.values))]
+    pars = [(gene_df.iloc[:, i], data_df, pops, tests, cutoffs, do_trims, bootstrap, b_size, b_reps) for i in
+            range(len(gene_df.columns.values))]
 
     # Loads the models for all the parameters parsed using multiprocessing to speed up computations
     pool = MyPool(processes=8)  # multiprocessing.cpu_count())
@@ -290,12 +323,13 @@ def mkt_on_df(gene_df, data_df, approach=None, pops=None, tests=None, cutoffs=No
     pool.terminate()
     results = pd.concat(results_list, axis=0, ignore_index=True)
 
-    if approach is not None: results['approach'] = approach
+    if label is not None: results['label'] = label
 
     return results
 
 
-def mkt_on_col(col, data_df, pops=None, tests=None, cutoffs=None, do_trims=None, bootstrap=None, b_size=100, b_reps=100):
+def mkt_on_col(col, data_df, pops=None, tests=None, cutoffs=None, do_trims=None, bootstrap=None, b_size=100,
+               b_reps=100):
     if do_trims is None:
         do_trims = [True, False]
     if cutoffs is None:
@@ -314,7 +348,8 @@ def mkt_on_col(col, data_df, pops=None, tests=None, cutoffs=None, do_trims=None,
         else:
             results = mkt_on_list(glist, data_df, pops, tests, cutoffs, do_trims)
     else:
-        results = {}
+        results = pd.DataFrame(index=[0])
+        results['nogenes'] = 0
 
     if col.name is not None:
         results['stage'] = col.name[0]
@@ -323,7 +358,6 @@ def mkt_on_col(col, data_df, pops=None, tests=None, cutoffs=None, do_trims=None,
 
 
 def bootstrap_on_list(glist, data_df, pops=None, tests=None, cutoffs=None, do_trims=None, b_size=100, b_reps=100):
-
     pars = [(np.random.choice(glist, b_size), data_df, pops, tests, cutoffs, do_trims) for _ in range(b_reps)]
 
     pool = MyPool(processes=4)  # multiprocessing.cpu_count())
@@ -394,7 +428,10 @@ def mkt_on_daf(daf, div, pop, nogenes, test, par):
             else:
                 xlow = 0
                 xhigh = 1
-            results = copy.deepcopy(aMKT(daf, div, xlow, xhigh))
+            results = copy.deepcopy(aMKT(daf, div, xlow, xhigh, check='raise'))
+            if 'alpha' not in results.keys():  # Uses covar matrix even if it's not valid, leads to bad CI's
+                results = copy.deepcopy(aMKT(daf, div, xlow, xhigh, check='ignore'))
+
             results = pd.DataFrame(results, index=[0])
             label_col = 'trim'
         else:
