2660c0ef3f0cab6690451182d8b6ccb2fff4317f
Xoel Mato Blanco <xoelmb@gmail.com>
2020-10-08 12:41:00 +0200

    class v1

--- rebase/MKT.py
+++ rebase/MKT.py
@@ -10,14 +10,14 @@ class MKT:
     """
     columns = ['alpha', 'omega', 'Ka', 'Ks']
     dtypes = 'float'
-    populations = ['AFR', 'EUR', 'EAS', 'SAS']
-    tests = ['eMKT', 'aMKT']
-    thresholds = [[0.05, 0.15], [0, 0.1]]
+    populations_dft = ['AFR', 'EUR', 'EAS', 'SAS']
+    tests_dft = ['eMKT', 'aMKT']
+    thresholds_dft = [[0.05, 0.15], [0, 0.1]]
 
     
     def __init__(self, genes, poldiv):
-        self.g = self._get_genes(genes)
-        self.ph = poldiv
+        self.genesets = self._get_genes(genes)
+        self.poldiv = poldiv
         self.results = pd.DataFrame(index=[], columns=self.columns, dtype=self.dtypes)
         self.result_list = []
 
@@ -30,7 +30,7 @@ class MKT:
             return genes.apply(self._col_to_array, axis=0).T.values
 
         if isinstance(genes, np.array):
-            return np.array(genes, str)
+            return np.array(('unnamed', np.array(genes, str)))
         
         else:
             raise TypeError('df or np.array(strings)')
@@ -40,14 +40,19 @@ class MKT:
         return np.array(('_'.join(col.name), np.array(col.index[col == 1].values, str)))
 
     
-    def test(self, genes=self.g, data=self.ph, tests=self.tests, thresholds=self.thresholds, populations=self.populations, label=None):
-        
+    def test(self, genes=None, data=None, tests=None, thresholds=None, populations=None, label=None):
+        genes = self.genesets if not genes else genes
+        data = self.poldiv if not data else data
+        tests = self.tests_dft if not tests else tests
+        thresholds = self.thresholds_dft if not thresholds else thresholds
+        populations = self.populations_dft if not populations else populations
+    
         self.last_result = mktest(genes, data, tests, thresholds, populations)
 
         if label:
             self.last_result['label'] = label
         
-        self.update_results(self.last_result)
+        self._update_results(self.last_result)
 
         return self.last_result
 
@@ -58,24 +63,24 @@ class MKT:
         self.result_list.append(new)
 
 
-    def amkt(self, genes=self.g, data=self.ph, thresholds=self.thresholds, populations=self.populations, label=None):
+    def amkt(self, genes=None, data=None, thresholds=None, populations=None, label=None):
 
-        return test(genes=genes, data=data, tests='aMKT', thresholds=thresholds, populations=populations, label=label)
-
-
-    def emkt(self, genes=self.g, data=self.ph, thresholds=self.thresholds, populations=self.populations, label=None):
-
-        return test(genes=genes, data=data, tests='eMKT', thresholds=thresholds, populations=populations, label=label)
+        return self.test(genes=genes, data=data, tests='aMKT', thresholds=thresholds, populations=populations, label=label)
 
 
-    def bootstrap(self, n=599, genes=self.g, data=self.ph, tests=self.tests, thresholds=self.thresholds, populations=self.populations, label=None):
+    def emkt(self, genes=None, data=None, thresholds=None, populations=None, label=None):
 
-        bs_genes = np.array([aux_bs(geneset, n=n) for geneset in genes])
+        return self.test(genes=genes, data=data, tests='eMKT', thresholds=thresholds, populations=populations, label=label)
+
+
+    def bootstrap(self, n=599, (self, genes=None, data=None, thresholds=None, populations=None, label=None):
+
+        bs_genes = np.array([_aux_bs(geneset, n=n) for geneset in genes])
         
-        return test(genes=bs_genes, data=data, tests=['eMKT', 'aMKT'], thresholds=thresholds, populations=populations, label=label)
+        return self.test(genes=bs_genes, data=data, tests=['eMKT', 'aMKT'], thresholds=thresholds, populations=populations, label=label)
         
 
-    def aux_bs(geneset, n=599):
+    def _aux_bs(self, geneset, n=599):
 
         bs_geneset = [np.array((geneset[0], np.random.choice(geneset[1],
                     size=len(geneset[1]), replace=True))) for _ in range(n)]
